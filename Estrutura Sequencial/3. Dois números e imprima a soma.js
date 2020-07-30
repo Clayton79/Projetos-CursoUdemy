b43bff762368c79c016c7b676a03a7240d5a0e6f
Clayton Antonio <claytonxavier21s@gmail.com>
2020-07-29 22:07:42 -0300

    Novos exercicios

--- Estrutura Sequencial/3. Dois números e imprima a soma.js
+++ Estrutura Sequencial/3. Dois números e imprima a soma.js
@@ -6,8 +6,8 @@ const Numero2 = 2
 
 const SomaTotal = Numero1+Numero2
 // Print Console.
-console.log ("Numero 1: "+ Numero1) 
+console.log ("Numero 1: " + Numero1) 
 
-console.log ("Numero 2: "+ Numero2)
+console.log ("Numero 2: " + Numero2)
 
-console.log ("resultado é :"+ SomaTotal)
+console.log ("resultado é :" + SomaTotal)
